fd5ce0174c3baeacca830b6e997e0fc6546ed9e0
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-12-30 17:54:15 +0100

    Using load instead of preload

--- GodotTogether.gd
+++ GodotTogether.gd
@@ -2,7 +2,7 @@
 extends BetterEditorPlugin_GodotTogether
 class_name GodotTogether
 
-const user_3d_scene = preload("res://addons/GodotTogether/assets/scenes/User3D/User3D.tscn")
+var user_3d_scene = load("res://addons/GodotTogether/assets/scenes/User3D/User3D.tscn")
 
 func _enter_tree():
 	create_user_3d()
