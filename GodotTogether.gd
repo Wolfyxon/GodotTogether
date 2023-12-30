c1744d3e2318962bdc333eec5da5f876063f3271
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-12-30 17:58:27 +0100

    Added User2D scene

--- GodotTogether.gd
+++ GodotTogether.gd
@@ -3,6 +3,7 @@ extends BetterEditorPlugin_GodotTogether
 class_name GodotTogether
 
 var user_3d_scene = load("res://addons/GodotTogether/assets/scenes/User3D/User3D.tscn")
+var user_2d_scene = load("res://addons/GodotTogether/assets/scenes/User2D/User2D.tscn")
 
 func _enter_tree():
 	create_user_3d()
@@ -17,3 +18,4 @@ func create_user_3d() -> User3D:
 	usr.main = self
 	add_child(usr)
 	return usr
+
