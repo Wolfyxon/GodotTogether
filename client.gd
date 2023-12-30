90d80fe30d384c14dc1178fca43d4bbe5f280a37
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-12-30 18:47:29 +0100

    Initial client logic

--- client.gd
+++ client.gd
@@ -1,2 +1,12 @@
 extends Node
 class_name GodotTogetherClient
+
+var peer = ENetMultiplayerPeer.new()
+
+func join(ip:String, port:int):
+	var err = peer.create_client(ip, port)
+	if err: return err
+	multiplayer.multiplayer_peer = peer
+
+func leave():
+	multiplayer.multiplayer_peer = null
