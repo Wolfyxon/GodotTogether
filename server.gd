11c6658c069de3e53ac3b9441d632e3387074137
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-12-30 18:30:45 +0100

    Initial server logic

--- server.gd
+++ server.gd
@@ -1,2 +1,12 @@
 extends Node
 class_name GodotTogetherServer
+
+var peer = ENetMultiplayerPeer.new()
+
+func start_hosting(port:int, max_clients:=10):
+	var err = peer.create_server(port, max_clients)
+	if err: return err
+	multiplayer.multiplayer_peer = peer
+
+func stop_hosting():
+	multiplayer.multiplayer_peer = null
