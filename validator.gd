eaab1e8f234e1bd5ef76995da34aff3f1ebf7292
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-12-31 12:52:24 +0100

    Added username validation

--- validator.gd
+++ validator.gd
@@ -1,2 +1,16 @@
 extends Object
 class_name GodotTogetherValidator
+
+const max_username_length = 32
+
+enum UsernameError {
+	OK,
+	TOO_LONG,
+	EMPTY,
+}
+
+static func validate_username(username:String) -> UsernameError:
+	if username.length() > max_username_length: return UsernameError.TOO_LONG
+	if username.replace(" ","").length() == 0: return UsernameError.EMPTY
+	
+	return UsernameError.OK
