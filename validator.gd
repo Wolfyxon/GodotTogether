b1385300597a4f83dc9ca8afbcb1057ce4a43c2e
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-12-31 12:56:20 +0100

    Added version validation

--- validator.gd
+++ validator.gd
@@ -9,6 +9,18 @@ enum UsernameError {
 	EMPTY,
 }
 
+enum VersionError {
+	OK,
+	UPDATE_REQUIRED,
+	TARGET_TOO_OLD,
+}
+
+static func validate_version(target_version:int):
+	if target_version > GodotTogether.compatibility_version: return VersionError.UPDATE_REQUIRED
+	if target_version < GodotTogether.compatibility_version: return VersionError.TARGET_TOO_OLD
+	
+	return VersionError.OK
+
 static func validate_username(username:String) -> UsernameError:
 	if username.length() > max_username_length: return UsernameError.TOO_LONG
 	if username.replace(" ","").length() == 0: return UsernameError.EMPTY
