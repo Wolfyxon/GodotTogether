1f3293fddfa093b5df026010558e69f13b81b26e
Wolfyxon <szymon.sajmon.b@gmail.com>
2023-12-31 23:08:22 +0100

    Moved tabs to a container

--- assets/scenes/GUI/MainMenu/MainMenu.gd
+++ assets/scenes/GUI/MainMenu/MainMenu.gd
@@ -4,13 +4,13 @@ class_name GodotTogetherMainMenu
 
 var main:GodotTogether
 
-@onready var join_ip = $tabs/start/vbox/type/join/address/ip
-@onready var join_port = $tabs/start/vbox/type/join/address/port
-@onready var join_password = $tabs/start/vbox/type/join/password
+@onready var join_ip = $tabs/start/vbox/joinOrHost/tabs/join/address/ip
+@onready var join_port = $tabs/start/vbox/joinOrHost/tabs/join/address/port
+@onready var join_password = $tabs/start/vbox/joinOrHost/tabs/join/password
 
-@onready var host_port = $tabs/start/vbox/type/host/smallConfig/port
-@onready var host_max_clients = $tabs/start/vbox/type/host/smallConfig/maxClients
-@onready var host_password = $tabs/start/vbox/type/host/password
+@onready var host_port = $tabs/start/vbox/joinOrHost/tabs/host/smallConfig/port
+@onready var host_max_clients = $tabs/start/vbox/joinOrHost/tabs/host/smallConfig/maxClients
+@onready var host_password = $tabs/start/vbox/joinOrHost/tabs/host/password
 
 func _ready():
 	pass
@@ -19,7 +19,7 @@ func _process(delta):
 	if not Engine.is_editor_hint(): popup()
 	if Engine.is_editor_hint() and EditorInterface.get_edited_scene_root() == self: return
 
-	$tabs/start/vbox/type/join/btnJoin.disabled = (join_ip.text == "")
+	$tabs/start/vbox/joinOrHost/tabs/join/btnJoin.disabled = (join_ip.text == "")
 
 func toggle():
 	if not visible: popup()
