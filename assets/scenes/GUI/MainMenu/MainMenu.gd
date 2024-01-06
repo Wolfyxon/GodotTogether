20f237f121dd58c8239d7be989b076b04e613c30
Wolfyxon <szymon.sajmon.b@gmail.com>
2024-01-06 15:00:49 +0100

    Error will be printed only if it's not null

--- assets/scenes/GUI/MainMenu/MainMenu.gd
+++ assets/scenes/GUI/MainMenu/MainMenu.gd
@@ -36,14 +36,14 @@ func _on_btn_host_pressed():
 	if main.is_session_active(): return
 	
 	var err = main.server.start_hosting(host_port.value, host_max_clients.value)
-	push_error("Cannot host. Err: "+str(err))
+	if err: push_error("Cannot host. Err: "+str(err))
 
 func _on_btn_join_pressed():
 	if not main: return
 	if main.is_session_active(): return
 	
 	var err = main.client.join(join_ip.text,join_port.value)
-	push_error("Cannot join. Err: "+str(err))
+	if err: push_error("Cannot join. Err: "+str(err))
 	
 
 func _on_btn_stop_pressed():
