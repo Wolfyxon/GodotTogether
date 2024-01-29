dc7d350a0e7e5c260b1ccff9d78160d56d6dfc1f
Wolfyxon <szymon.sajmon.b@gmail.com>
2024-01-29 09:36:51 +0100

    Added connect and disconnect event listeners

--- server.gd
+++ server.gd
@@ -3,7 +3,19 @@ class_name GodotTogetherServer
 
 var peer = ENetMultiplayerPeer.new()
 
+func _ready():
+	peer.peer_connected.connect(_connected)
+	peer.peer_disconnected.connect(_disconnected)
+
+
 func start_hosting(port:int, max_clients:=10):
 	var err = peer.create_server(port, max_clients)
 	if err: return err
 	multiplayer.multiplayer_peer = peer
+
+func _connected(id: int):
+	var peer = peer.get_peer(id)
+	print("Peer: "+str(id)+" connected from "+peer.get_remote_address())
+
+func _disconnected(id:int ):
+	print("Peer "+str(id)+" disconnected")
