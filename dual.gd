39ffb5094935df7306101b1d507f430adbaa9591
Wolfyxon <szymon.sajmon.b@gmail.com>
2024-01-29 12:48:39 +0100

    Mouse marker updating logic

--- dual.gd
+++ dual.gd
@@ -3,3 +3,20 @@ extends Node
 class_name GodotTogetherDual
 
 var main:GodotTogether
+var update_timer = Timer.new()
+
+func _ready():
+	update_timer.timeout.connect(_update)
+	update_timer.one_shot = false
+	update_timer.wait_time = 0.02
+	add_child(update_timer)
+	update_timer.start()
+
+func _update():
+	if not main: return
+	rpc("update_2d_marker", get_viewport().get_mouse_position())
+
+@rpc("any_peer")
+func update_2d_marker(vector: Vector2):
+	if not main: return
+	main.get_user_2d(multiplayer.get_remote_sender_id()).set_position_percent(vector)
