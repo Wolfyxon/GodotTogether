878b47b07e5bde089127ea01a09a016264225a4b
Wolfyxon <szymon.sajmon.b@gmail.com>
2024-01-29 15:47:54 +0100

    3D marker updating

--- dual.gd
+++ dual.gd
@@ -3,12 +3,16 @@ extends Node
 class_name GodotTogetherDual
 
 var main:GodotTogether
+var camera:Camera3D
 var update_timer = Timer.new()
 
 var prev_mouse_pos := Vector2()
+var prev_3d_pos := Vector3()
+var prev_3d_rot := Vector3()
 
 func _ready():
 	if not main: return
+	camera = main.get_editor_3d_camera()
 	
 	multiplayer.peer_connected.connect(_connected)
 	multiplayer.peer_disconnected.connect(_disconnected)
@@ -27,6 +31,12 @@ func _update():
 	if mPos != prev_mouse_pos and DisplayServer.window_is_focused():
 		prev_mouse_pos = mPos
 		update_2d_marker.rpc(mPos)
+		
+	if camera.position != prev_3d_pos or camera.rotation != prev_3d_rot:
+		prev_3d_pos = camera.position
+		prev_3d_rot = camera.rotation
+		
+		update_3d_marker.rpc(camera.position, camera.rotation)
 
 func _connected(id:int):
 	pass
@@ -40,3 +50,12 @@ func update_2d_marker(vector: Vector2):
 	var marker = main.get_user_2d(multiplayer.get_remote_sender_id())
 	if not marker: return
 	marker.set_position_percent(vector)
+
+@rpc("any_peer")
+func update_3d_marker(position:Vector3, rotation:Vector3):
+	if not main: return
+	var marker = main.get_user_3d(multiplayer.get_remote_sender_id())
+	if not marker: return
+	marker.position = position
+	marker.rotation = rotation
+
