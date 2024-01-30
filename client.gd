ed6657e32d01bbe9761d9a1a9537cbe2615b9bcc
Wolfyxon <szymon.sajmon.b@gmail.com>
2024-01-30 11:30:02 +0100

    Added userdata sending logic

--- client.gd
+++ client.gd
@@ -4,9 +4,11 @@ class_name GodotTogetherClient
 var main:GodotTogether
 var peer = ENetMultiplayerPeer.new()
 
-func join(ip:String, port:int):
+func join(ip:String, port:int, data := {}):
 	var err = peer.create_client(ip, port)
 	if err: return err
 	multiplayer.multiplayer_peer = peer
 	
 	print("Connected, your ID is: "+str(multiplayer.get_unique_id()))
+	main.server.receive_user_data.rpc_id(1, data)
+	print("Userdata sent")
