1591d1cedec7788b4f1b54d7af70e84293e6c339
Wolfyxon <szymon.sajmon.b@gmail.com>
2024-02-02 20:28:27 +0100

    Added get_property_keys()

--- changeDetector.gd
+++ changeDetector.gd
@@ -6,3 +6,10 @@ var main:GodotTogether
 
 signal node_property_changed
 
+static func get_property_keys(node:Node) -> Array[String]:
+	var res = []
+	
+	for i in node.get_property_list():
+		res.append(i.name)
+		
+	return res
