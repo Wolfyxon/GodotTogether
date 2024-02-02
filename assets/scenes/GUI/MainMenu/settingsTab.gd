06a25b92e2173ad52b30751de196333f22607f45
Wolfyxon <szymon.sajmon.b@gmail.com>
2024-02-02 18:09:31 +0100

    Added settings loading logic

--- assets/scenes/GUI/MainMenu/settingsTab.gd
+++ assets/scenes/GUI/MainMenu/settingsTab.gd
@@ -1,19 +1,31 @@
 @tool
 extends VBoxContainer
 
+var booleans:Array[Button]
+var strings:Array[Control]
+
 func _ready():
 	register_bool($server/enableWhitelist, "server/whitelist_enabled")
 	register_bool($server/allowExt, "server/allow_external_connections")
 
+func load_settings():
+	for i in strings:
+		i.text = GodotTogetherSettings.get_setting(i.get_meta("path"))
+	
+	for i in booleans:
+		i.button_pressed = GodotTogetherSettings.get_setting(i.get_meta("path"))
 
 func register_bool(button:Button, path:String):
 	if not button.toggle_mode:
 		push_warning(button.name, " is not togglelable")
 	
+	button.set_meta("path", path)
 	button.toggled.connect(_set_bool.bind(button, path))
 
 func register_text(input: Control, path:String):
 	assert(input is LineEdit or input is TextEdit, "Not a LineEdit or TextEdit")
+	
+	input.set_meta("path", path)
 	input.text_changed.connect(_set_text.bind(input, path))
 
 
