0a1b1fd1fc3a5df156a116dad803157b5668f003
Wolfyxon <szymon.sajmon.b@gmail.com>
2024-02-02 20:32:27 +0100

    Added properties_to_dict()

--- changeDetector.gd
+++ changeDetector.gd
@@ -4,7 +4,7 @@ class_name GodotTogetherChangeDetector
 
 var main:GodotTogether
 
-signal node_property_changed
+signal node_properties_changed
 
 static func get_property_keys(node:Node) -> Array[String]:
 	var res = []
@@ -12,4 +12,12 @@ static func get_property_keys(node:Node) -> Array[String]:
 	for i in node.get_property_list():
 		res.append(i.name)
 		
+	return res
+
+static func properties_to_dict(node:Node) -> Dictionary:
+	var res = {}
+	
+	for i in get_property_keys(node):
+		res[i] = node[i]
+	
 	return res
