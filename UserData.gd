398f4668a7917ff283e053c2d70d382f3dc69962
Wolfyxon <wolfyxon@gmail.com>
2024-05-14 13:56:09 +0200

    Added user data class

---
+++ UserData.gd
@@ -0,0 +1,20 @@
+extends Object
+class_name GodotTogetherUserData
+
+var username: String
+var permission_level: GodotTogetherServer.PermissionLevel = GodotTogetherServer.PermissionLevel.GUEST
+
+func _init(username: String, permission_level: GodotTogetherServer.PermissionLevel = GodotTogetherServer.PermissionLevel.GUEST):
+	self.username = username
+	self.permission_level = permission_level
+
+# WARNING: Do not allow users to load their data
+func load_data(data: Dictionary):
+	var username: String = data["username"]
+	var perm_lvl: GodotTogetherServer.PermissionLevel = data["permission_level"]
+	
+	assert(username, "Data citionary does not specify the username")
+	if not perm_lvl: perm_lvl = permission_level
+	
+	self.username = username
+	permission_level = perm_lvl
