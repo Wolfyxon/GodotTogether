f0eea0fd8a0419d23bd03dca3e4f0f458a16094b
Wolfyxon <wolfyxon@gmail.com>
2024-06-26 10:43:29 +0200

    Changes are detected in a loop

--- src/scripts/changeDetector.gd
+++ src/scripts/changeDetector.gd
@@ -2,7 +2,7 @@
 extends Node
 class_name GodotTogetherChangeDetector
 
-signal node_properties_changed(node: Node, changed_keys: String)
+signal node_properties_changed(node: Node, changed_keys: Array[String])
 signal node_property_changed(node: Node, key: String)
 signal node_property_differs(node: Node, key: String, old_value, new_value)
 
@@ -39,30 +39,52 @@ static func get_property_dict(node: Node) -> Dictionary:
 	
 	return res
 
+var observed_nodes_cache = {}
+
+func _process(_delta):
+	for node in observed_nodes:
+		if not is_instance_valid(node): continue
+		
+		var cached = observed_nodes_cache[node]
+		var current = get_property_dict(node)
+		
+		var changed_keys = []
+		
+		for i in current.keys():
+			if cached[i] != current[i]:
+				node_property_changed.emit(node, i)
+				node_property_differs.emit(node, i, cached[i], current[i])
+				changed_keys.append(i)
+				
+		if changed_keys.size() != 0:
+			node_properties_changed.emit(node, changed_keys)
+	
 
 func observe(node: Node):
 	if node in observed_nodes: return
+	observed_nodes_cache[node] = get_property_dict(node)
 	observed_nodes.append(node)
 	
-	var cache = get_property_dict(node)
-	
-	var on_change = func():
-		var changed_keys = []
-		var current = get_property_dict(node)
-		
-		for i in current.keys():
-			if cache[i] != current[i]:
-				node_property_changed.emit(node, i)
-				node_property_differs.emit(node, i, cache[i], current[i])
-				changed_keys.append(i)
-		
-		node_properties_changed.emit(node, changed_keys)
-		cache = current
-	
-	node.property_list_changed.connect(on_change)
-	node.tree_exiting.connect(func():
-		node.property_list_changed.disconnect(on_change)
-	)
+	# property_list_changed doesn't fire in editor
+	#var cache = get_property_dict(node)
+	#
+	#var on_change = func():
+		#var changed_keys = []
+		#var current = get_property_dict(node)
+		#
+		#for i in current.keys():
+			#if cache[i] != current[i]:
+				#node_property_changed.emit(node, i)
+				#node_property_differs.emit(node, i, cache[i], current[i])
+				#changed_keys.append(i)
+		#
+		#node_properties_changed.emit(node, changed_keys)
+		#cache = current
+	#
+	#node.property_list_changed.connect(on_change)
+	#node.tree_exiting.connect(func():
+		#node.property_list_changed.disconnect(on_change)
+	#)
 
 func observe_recursive(node: Node):
 	observe(node)
