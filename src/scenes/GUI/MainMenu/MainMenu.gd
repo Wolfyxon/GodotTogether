d2f3abf9dfddd6a4ce5330574a669f02f7828d3e
Wolfyxon <wolfyxon@gmail.com>
2024-06-29 11:19:19 +0200

    Put buttons in vars

--- src/scenes/GUI/MainMenu/MainMenu.gd
+++ src/scenes/GUI/MainMenu/MainMenu.gd
@@ -9,10 +9,12 @@ var main: GodotTogether
 @onready var join_ip = $tabs/start/vbox/joinOrHost/tabs/join/address/ip
 @onready var join_port = $tabs/start/vbox/joinOrHost/tabs/join/address/port
 @onready var join_password = $tabs/start/vbox/joinOrHost/tabs/join/password
+@onready var join_btn = $tabs/start/vbox/joinOrHost/tabs/join/btnJoin
 
 @onready var host_port = $tabs/start/vbox/joinOrHost/tabs/host/smallConfig/port
 @onready var host_max_clients = $tabs/start/vbox/joinOrHost/tabs/host/smallConfig/maxClients
 @onready var host_password = $tabs/start/vbox/joinOrHost/tabs/host/password
+@onready var host_btn = $tabs/start/vbox/joinOrHost/tabs/host/btnHost
 
 @onready var cover = $tabs/start/vbox/joinOrHost/cover
 
@@ -27,8 +29,8 @@ func _process(delta):
 	if not Engine.is_editor_hint() and not visible: popup()
 	if Engine.is_editor_hint() and EditorInterface.get_edited_scene_root() == self: return
 
-	$tabs/start/vbox/joinOrHost/tabs/join/btnJoin.disabled = (join_ip.text == "") or cover.visible
-	$tabs/start/vbox/joinOrHost/tabs/host/btnHost.disabled = cover.visible
+	join_btn.disabled = (join_ip.text == "") or cover.visible
+	host_btn.disabled = cover.visible
 	
 	cover.visible = (main and main.is_session_active())
 	
