b94ccbb165517a8d932a9f0163040d88ac96e5cf
Wolfyxon <wolfyxon@gmail.com>
2024-09-01 22:07:32 +0200

    Removed get_fs_hash()

--- src/scripts/files.gd
+++ src/scripts/files.gd
@@ -59,35 +59,3 @@ static func get_dirs(path: String) -> Array[String]:
 		file_name = dir.get_next()
 		
 	return res
-
-static func get_fs_hash(path := "res://") -> int:
-	var res = 0
-	
-	var dir = DirAccess.open(path)
-	assert(dir, "Failed to open " + path)
-	
-	dir.list_dir_begin()
-	var file_name = dir.get_next()
-	
-	while file_name != "":
-		if dir.current_is_dir():
-			var ignored = false
-			# TODO: Check absolute paths
-			for ignored_dir in ignored_dirs:
-				if path + ignored_dir == "res://" + ignored_dir:
-					ignored = true
-					break
-			if ignored: 
-				file_name = dir.get_next()
-				continue
-			
-			res += get_fs_hash(path + "/" + file_name)
-		else:
-			var f = FileAccess.open(path + "/" + file_name, FileAccess.READ)
-			res += hash(f.get_buffer(f.get_length()))
-			f.close()
-		
-		res += file_name.hash()
-		file_name = dir.get_next()
-	
-	return res
