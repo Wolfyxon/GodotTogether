60bd550f8ff39818739662619a5e11e612b6f541
Wolfyxon <wolfyxon@gmail.com>
2024-12-28 15:54:10 +0100

    Added Permission enum

--- src/scripts/net/user/User.gd
+++ src/scripts/net/user/User.gd
@@ -2,3 +2,12 @@
 
 ### Class for connected users
 class_name GodotTogetherUser
+
+enum Permission {
+    EDIT_SCRIPTS,
+    EDIT_SCENES,
+    DELETE_SCENES,
+    DELETE_SCRIPTS,
+    ADD_CUSTOM_FILES,
+    MODIFY_CUSTOM_FILES
+}
