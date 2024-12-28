d7592f12796448659943061dedec444a9490a58b
Wolfyxon <wolfyxon@gmail.com>
2024-12-28 16:49:26 +0100

    Moved Permission enum to the main plugin script

--- src/scripts/net/user/User.gd
+++ src/scripts/net/user/User.gd
@@ -3,19 +3,10 @@
 ### Class for connected users
 class_name GodotTogetherUser
 
-enum Permission {
-    EDIT_SCRIPTS,
-    EDIT_SCENES,
-    DELETE_SCENES,
-    DELETE_SCRIPTS,
-    ADD_CUSTOM_FILES,
-    MODIFY_CUSTOM_FILES
-}
-
 var id: int
 var username: String
 var peer: ENetPacketPeer
-var permissions: Array[Permission] = []
+var permissions: Array[GodotTogether.Permission] = []
 var authenticated := false
 
 func _init(id: int, peer: ENetPacketPeer):
