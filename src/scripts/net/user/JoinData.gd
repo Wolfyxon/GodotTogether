91d3e2a3664391bb827ce0100ead5611a21d5b4c
Wolfyxon <wolfyxon@gmail.com>
2024-12-28 15:52:04 +0100

    Added username and as_dict()

--- src/scripts/net/user/JoinData.gd
+++ src/scripts/net/user/JoinData.gd
@@ -2,3 +2,10 @@
 
 ### Class for data sent by users upon joining 
 class_name GodotTogetherJoinData
+
+var username: String
+
+func as_dict() -> Dictionary:
+    return {
+        "username": username
+    }
