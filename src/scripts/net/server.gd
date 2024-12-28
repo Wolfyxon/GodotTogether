f9e9ebe72876db17ac06f3bb9726e589c4c001a8
Wolfyxon <wolfyxon@gmail.com>
2024-12-28 16:18:40 +0100

    Readded some basic functions

--- src/scripts/net/server.gd
+++ src/scripts/net/server.gd
@@ -1,2 +1,38 @@
 @tool
+extends Node
 class_name GodotTogetherServer
+
+var main: GodotTogether
+var server_peer = ENetMultiplayerPeer.new()
+
+func _ready():
+	multiplayer.peer_connected.connect(_connected)
+	multiplayer.peer_disconnected.connect(_disconnected)
+
+func _connected(id: int):
+	if not multiplayer.is_server(): return
+
+func _disconnected(id: int):
+	if not multiplayer.is_server(): return
+
+func is_active() -> bool:
+	return server_peer.get_connection_status() == MultiplayerPeer.CONNECTION_CONNECTED
+
+static func is_local(ip: String) -> bool:
+	if ip in ["0:0:0:0:0:0:0:1", "127.0.0.1", ":1", "localhost"]: return true
+	
+	var split = ip.split(".")
+	if split.size() != 4:
+		push_error(ip + " doesn't seem to be a valid IP address: size not equal to 4. Assuming this is not a local address.")
+		return false
+	
+	var a = int(split[0])
+	var b = int(split[1])
+	var c = int(split[2])
+	var d = int(split[3])
+	
+	if a == 127: return true
+	if a == 172 and b >= 16 and b <= 31: return true
+	if a == 192 and b == 168: return true
+	
+	return false
