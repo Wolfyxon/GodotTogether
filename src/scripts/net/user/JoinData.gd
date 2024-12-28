6469d5f0ee79d9dfaa0680c8b6d79f36f059b6cd
Wolfyxon <wolfyxon@gmail.com>
2024-12-28 16:05:35 +0100

    Added from_dict()

--- src/scripts/net/user/JoinData.gd
+++ src/scripts/net/user/JoinData.gd
@@ -9,3 +9,14 @@ func as_dict() -> Dictionary:
     return {
         "username": username
     }
+
+static func from_dict(dict: Dictionary) -> GodotTogetherJoinData:
+    var res = GodotTogetherJoinData.new()
+
+    for key in dict.keys():
+        var val = dict[key]
+        assert(val != null, "Invalid user data: %s is null: %s" % [key, dict])
+
+    res.username = dict["username"]
+
+    return res
