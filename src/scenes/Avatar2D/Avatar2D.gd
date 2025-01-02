92c2c8c5a8152c84847c5def01d19dbee6e332a4
Wolfyxon <wolfyxon@gmail.com>
2025-01-02 12:23:07 +0100

    Added set_user()

--- src/scenes/Avatar2D/Avatar2D.gd
+++ src/scenes/Avatar2D/Avatar2D.gd
@@ -9,5 +9,9 @@ var id := -1
 func set_username(name: String):
 	txt.get_node("name").text = name
 
+func set_user(user: GodotTogetherUser):
+	id = user.id
+	txt.get_node("name").text = user.name
+
 func set_position_percent(vector: Vector2):
 	global_position = Vector2(EditorInterface.get_editor_viewport_2d().size) * Vector2(vector)
