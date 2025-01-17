9d18cdcd2cc43b60362f8a5faadc592d2d26549f
Wolfyxon <wolfyxon@gmail.com>
2025-01-17 22:42:19 +0100

    Added main_menu()

--- src/scenes/GUI/NewMenu/menu.gd
+++ src/scenes/GUI/NewMenu/menu.gd
@@ -7,11 +7,16 @@ var main: GodotTogether
 func _ready() -> void:
 	if not visuals_available(): return
 	
+	main_menu()
 	$about/scroll/vbox/version.text = "Version: " + GodotTogether.VERSION
 
 func _process(delta: float) -> void:
 	if not Engine.is_editor_hint():
 		visible = true
+
+func main_menu() -> void:
+	$main/sessionInit/start.visible = false
+	$main/sessionInit/pre.visible = true
 
 func visuals_available() -> bool:
 	return main or not Engine.is_editor_hint() 
