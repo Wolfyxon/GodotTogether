b19386c5d2b0f645558902868f822ab0307cbaaf
Wolfyxon <wolfyxon@gmail.com>
2025-01-17 22:03:00 +0100

    Setting version text

--- src/scenes/GUI/NewMenu/menu.gd
+++ src/scenes/GUI/NewMenu/menu.gd
@@ -2,6 +2,16 @@
 extends PopupPanel
 class_name GodotTogetherMenu
 
+var main: GodotTogether
+
+func _ready() -> void:
+	if not visuals_available(): return
+	
+	$about/scroll/vbox/version.text = "Version: " + GodotTogether.VERSION
+
 func _process(delta: float) -> void:
 	if not Engine.is_editor_hint():
 		visible = true
+
+func visuals_available() -> bool:
+	return main or not Engine.is_editor_hint() 
