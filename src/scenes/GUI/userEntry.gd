b154c9a3e21666bb1a0071307df1af69c653ce59
Wolfyxon <wolfyxon@gmail.com>
2025-02-02 16:44:46 +0100

    Init user list logic

---
+++ src/scenes/GUI/userEntry.gd
@@ -0,0 +1,8 @@
+@tool
+extends HBoxContainer
+class_name GodotTogetherGUIUser
+
+var user: GodotTogetherUser
+
+func set_user(user: GodotTogetherUser):
+	pass
---
+++ src/scenes/GUI/users.gd
@@ -0,0 +1,37 @@
+@tool
+extends ScrollContainer
+
+var main: GodotTogether
+
+var template = $vbox/user
+
+func add_user(user: GodotTogetherUser):
+	if get_entry(user):
+		push_warning("User %s alreay on the list" % user.id)
+		return
+	
+	var clone: GodotTogetherGUIUser = $vbox/user.duplicate()
+	clone.name = str(user.id)
+	
+	clone.set_user(user)
+	$vbox.add_child(clone)
+
+func get_entry(user: GodotTogetherUser) -> GodotTogetherGUIUser:
+	for i in get_entries():
+		if i.user == user:
+			return i
+		
+	return null
+
+func get_entries() -> Array[GodotTogetherGUIUser]:
+	var res: Array[GodotTogetherGUIUser] = []
+	
+	for i in $vbox.get_children():
+		if i != template and i is GodotTogetherGUIUser:
+			res.append(i)
+	
+	return res
+
+func clear():
+	for i in get_entries():
+		i.queue_free()
