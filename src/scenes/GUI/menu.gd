b8187e892f95f3bf50162d8102950a5467e7b08e
Wolfyxon <wolfyxon@gmail.com>
2025-07-17 13:09:15 +0200

    Init session start cover for indicating progress

--- src/scenes/GUI/menu.gd
+++ src/scenes/GUI/menu.gd
@@ -7,6 +7,7 @@ var gui: GodotTogetherGUI
 
 @onready var users: GodotTogetherUserList = $session/tabs/Users
 @onready var username_input = $sessionInit/pre/username
+@onready var session_init_cover = $sessionInit/cover
 
 func _ready() -> void:
 	await get_tree().process_frame
@@ -18,9 +19,11 @@ func _host() -> void:
 	if main:
 		var port = $sessionInit/start/host/port/value.value
 		var max_clients = $sessionInit/start/host/users/value.value
-
+		
+		set_session_init_cover("Starting server...")
+		
 		var err = main.server.start_hosting(port, max_clients)
-
+		
 		if err:
 			gui.alert("Failed to start server: %s" % error_string(err), "Failed to start server")
 			return
@@ -39,14 +42,18 @@ func _join() -> void:
 
 		var ip = $sessionInit/start/join/address/ip.text
 		var port = $sessionInit/start/join/address/port.value
-
+		
+		set_session_init_cover("Connecting...")
+		
 		var err = main.client.join(ip, port, main.client.current_join_data)
 		
 		if err:
+			set_session_init_cover()
 			gui.alert("Failed to create client: %s" % err)
 			return
-
+		
 		if not await main.client.connecting_finished:
+			set_session_init_cover()
 			gui.alert("Failed to connect to %s:%s" % [ip, port])
 			return
 
@@ -58,6 +65,14 @@ func _joined() -> void:
 	$session/top/status.text = "Connected"
 	$session/top/end.text = "Disconnect"
 
+
+func set_session_init_cover(text: String = ""):
+	if text == "":
+		session_init_cover.hide()
+		return
+	
+	session_init_cover.get_node("vbox/title").text = text
+	session_init_cover.show()
 
 func end_session() -> void:
 	if main and main.is_session_active():
@@ -79,6 +94,8 @@ func main_menu() -> void:
 	$session.hide()
 
 func session_start_menu():
+	set_session_init_cover()
+	
 	$sessionInit/start.show()
 	$sessionInit/pre.hide()
 	# Layout glitch fix
