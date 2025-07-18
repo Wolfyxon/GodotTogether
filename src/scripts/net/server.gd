cfbf503fa35ec5bed9c8734facfcbf99cddf2f4e
Wolfyxon <wolfyxon@gmail.com>
2025-07-18 20:52:35 +0200

    Fixed name not setting on the server side

--- src/scripts/net/server.gd
+++ src/scripts/net/server.gd
@@ -91,9 +91,13 @@ func start_hosting(port: int, max_clients := 10) -> int:
 		create_server_user()	
 	]
 
-	main.dual.users_listed.emit(connected_users)
+	_post_start()
 
 	return err
+
+func _post_start() -> void:
+	await get_tree().process_frame
+	main.dual.users_listed.emit(connected_users)
 
 func id_has_permission(peer_id: int, permission: GodotTogether.Permission) -> bool:
 	var user = get_user_by_id(peer_id)
