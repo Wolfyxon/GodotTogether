fff3bbe7ce4b135eb143391163b5b0405b39c209
Wolfyxon <wolfyxon@gmail.com>
2025-08-01 13:59:52 +0200

    Setting panel style from the editor

--- src/scenes/GUI/chat/chat.gd
+++ src/scenes/GUI/chat/chat.gd
@@ -3,3 +3,10 @@ extends VBoxContainer
 class_name GDTChat
 
 var main: GodotTogether
+
+func _ready() -> void:
+	if not main: return
+	
+	var scroll_style = EditorInterface.get_editor_theme().get_stylebox("panel", "Panel")
+	
+	$scroll.add_theme_stylebox_override("panel", scroll_style)
