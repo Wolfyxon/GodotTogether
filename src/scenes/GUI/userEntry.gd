faf4044b5f192eda16553e6c1d43c97d8ffa4d8b
zastinian <zastinian@gmail.com>
2025-10-19 14:14:07 -0600

    feat: Update user entry

--- src/scenes/GUI/userEntry.gd
+++ src/scenes/GUI/userEntry.gd
@@ -3,50 +3,78 @@ extends HBoxContainer
 class_name GDTGUIUser
 
 var user: GDTUser
+var gui: GodotTogetherGUI
+var is_pending := false
 
-@onready var color_node = $color
-@onready var name_node = $name
-@onready var id_node = $id
-@onready var ip_node = $ip/value
-@onready var ip_toggle = $ip/toggle
-@onready var rank_node = $rank
-@onready var kick_btn = $actions/kick
-
-func _process(_delta: float) -> void:
-	if user and not user.is_peer_connected():
-		queue_free()
-
-func set_ip_visible(state: bool) -> void:
-	if not user.peer: return
-
-	ip_node.secret = not state
-	
-	if state:
-		ip_toggle.icon = GDTUserList.IMG_IP_VISIBLE
-	else:
-		ip_toggle.icon = GDTUserList.IMG_IP_HIDDEN
-
-func is_ip_visible() -> bool:
-	return not ip_node.secret
+func _ready() -> void:
+	var parent = get_parent()
+	while parent:
+		if parent is GDTUserList:
+			gui = parent.gui
+			is_pending = parent.is_pending_tab
+			break
+		parent = parent.get_parent()
 
 func set_user(user: GDTUser) -> void:
-	color_node.color = user.color
-	name_node.text = user.name
-	id_node.text = str(user.id)
-	
 	self.user = user
-
+	$color.color = user.color
+	$name.text = user.name
+	$id.text = str(user.id)
+	
 	if user.peer:
-		ip_node.text = user.peer.get_remote_address()
+		$ip/value.text = user.peer.get_remote_address()
 	else:
-		ip_toggle.disabled = true
-		ip_node.secret = false
-		ip_node.text = "N/A"
+		$ip/value.text = "Local"
 	
-	rank_node.select(user.type)
+	var rank: OptionButton = $rank
+	rank.selected = user.type
 	
-	kick_btn.disabled = not user.peer
-	kick_btn.pressed.connect(user.kick.bind(GDTUser.DisconnectReason.KICKED))
+	if is_pending:
+		setup_pending_actions()
+	else:
+		setup_normal_actions()
+
+func setup_pending_actions() -> void:
+	var actions = $actions
+	var kick_btn = $actions/kick
+	
+	kick_btn.text = "Approve"
+	kick_btn.pressed.disconnect(_on_kick_pressed)
+	kick_btn.pressed.connect(_on_approve_pressed)
+	
+	var reject_btn = Button.new()
+	reject_btn.text = "Reject"
+	reject_btn.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	reject_btn.pressed.connect(_on_reject_pressed)
+	actions.add_child(reject_btn)
+
+func setup_normal_actions() -> void:
+	if not $actions/kick.pressed.is_connected(_on_kick_pressed):
+		$actions/kick.pressed.connect(_on_kick_pressed)
+
+func set_ip_visible(state: bool) -> void:
+	$ip/value.secret = not state
+	
+	if state:
+		$ip/toggle.icon = GDTUserList.IMG_IP_VISIBLE
+	else:
+		$ip/toggle.icon = GDTUserList.IMG_IP_HIDDEN
 
 func toggle_ip_visibility() -> void:
-	set_ip_visible(not is_ip_visible())
+	set_ip_visible($ip/value.secret)
+
+func _on_kick_pressed() -> void:
+	if not gui or not gui.main: return
+	
+	if await gui.confirm("Are you sure you want to kick %s?" % user.name):
+		user.kick()
+
+func _on_approve_pressed() -> void:
+	if gui and gui.main:
+		gui.main.server.approve_pending_user(user)
+		queue_free()
+
+func _on_reject_pressed() -> void:
+	if gui and gui.main:
+		gui.main.server.reject_pending_user(user)
+		queue_free()
