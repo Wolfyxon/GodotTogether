6a5b321a83f628d1ea468173200e6442daeea566
AramCZ <aramcz@protonmail.com>
2026-07-18 18:15:07 +0200

    Move detect tool script to client.gd

--- src/scripts/net/client.gd
+++ src/scripts/net/client.gd
@@ -181,19 +181,23 @@ func receive_file(path: String, buffer: PackedByteArray) -> void:
 	
 	f.store_buffer(buffer)
 	
+	if path.get_extension() == "gd" and "@tool" in buffer.get_string_from_utf8():
+		var warning_message = "Tool script detected (%s). It can execute malicious code in your editor!" % path
+		print(warning_message)
+
+		if main and main.gui:
+			main.gui.alert(warning_message)
+	
 	print("Saved successfully")
 	
-	if path.get_extension() == "tscn":
-		var current_scene = EditorInterface.get_edited_scene_root()
+	if path.get_extension() == "gd" and "@tool" in buffer.get_string_from_utf8():
+		var warning_message = "Tool script detected (%s). It can execute malicious code in your editor!" % path
+		
+		push_warning(warning_message)
+		print(warning_message)
 
-		if current_scene and current_scene.scene_file_path == path:
-			EditorInterface.mark_scene_as_unsaved()
-
-		EditorInterface.reload_scene_from_path(path)
-
-	if target_file_count != 0 and downloaded_file_count >= target_file_count:
-		target_file_count = 0
-		_project_files_downloaded()
+		if main and main.gui:
+			main.gui.alert(warning_message)
 
 @rpc("authority", "call_remote", "reliable")
 func sync_file_add(path: String, buffer: PackedByteArray) -> void:
