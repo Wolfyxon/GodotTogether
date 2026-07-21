d4d843c668bdfd31f46d23b76ab4f57028748e1a
Wolfyxon <wolfyxon@gmail.com>
2026-07-21 12:13:41 +0200

    Added unit tests

---
+++ src/scripts/tests.gd
@@ -0,0 +1,86 @@
+extends GDTComponent
+class_name GDTUnitTests
+
+var success_count = 0
+var fail_count = 0
+
+func exec_test(f: Callable) -> void:
+	var res = f.call()
+	
+	if res:
+		print("%s: Ok" % f.get_method())
+		success_count += 1
+	else:
+		print("%s: FAIL" % f.get_method())
+		fail_count += 1
+
+func run_tests() -> void:
+	print("--- Running GodotTogether tests ---")
+	
+	exec_test(test_sha256)
+	exec_test(test_sha256_file)
+	
+	print()
+	print("Testing complete")
+	print("Succeed: %s | Failed: %s" % [success_count, fail_count])
+	
+	print("------------------------------------")
+
+func test_sha256() -> bool:
+	var a1 = [1, 2, 3, 4]
+	var a2 = [1, 2, 3, 4]
+	
+	var b = [1, 4, 8, 9]
+	
+	var hash_a1 = GDTUtils.sha256_of_buffer(a1)
+	var hash_a2 = GDTUtils.sha256_of_buffer(a2)
+	
+	if hash_a1.is_empty():
+		printerr("Hash is empty")
+		return false
+	
+	if hash_a1 != hash_a2:
+		printerr("Hashes of 'a' don't match")
+		return false
+	
+	if hash_a1 == GDTUtils.sha256_of_buffer(b):
+		printerr("Hashes of different values equal")
+		return false
+	
+	return true
+
+func test_sha256_file() -> bool:
+	var script = get_script()
+	
+	if not script:
+		printerr("Unable to get script instance")
+		return false
+	
+	var path: String = script.resource_path
+	
+	if path.is_empty():
+		printerr("Script instance not a file")
+		return false
+		
+	var buf = FileAccess.get_file_as_bytes(path)
+	
+	if buf.is_empty():
+		printerr("Unable to open file %s" % path)
+		return false
+	
+	var hash_buf = GDTUtils.sha256_of_buffer(buf)
+	var hash_file = GDTUtils.sha256_of_file(path)
+	
+	if hash_buf.is_empty():
+		printerr("sha256_of_buffer() empty")
+		return false
+	
+	if hash_file.is_empty():
+		printerr("sha256_of_file() empty")
+		return false
+		
+	if hash_buf != hash_file:
+		printerr("Hashes differ: \n%s\n%s" % [hash_file, hash_buf])
+		return false
+	
+	return true
