68658fa58394b6cf20c0b545e544910769d9b5f0
Wolfyxon <wolfyxon@gmail.com>
2026-09-05 14:23:43 +0200

    Better disclaimer screen

--- src/scenes/GUI/disclaimer.gd
+++ src/scenes/GUI/disclaimer.gd
@@ -2,7 +2,7 @@
 extends VBoxContainer
 class_name GDTDisclaimer
 
-@onready var checks = $checks.get_children()
+@onready var checks = []
 @onready var btn_proceed = $btnProceed
 
 var gui: GodotTogetherGUI
@@ -13,11 +13,19 @@ func _ready() -> void:
 	if not gui: return
 	if not gui.visuals_available(): return
 	
+	setup_checklist()
+	
 	for i: CheckBox in checks:
 		i.pressed.connect(_update)
 		
 	_update()
 
+func setup_checklist() -> void:
+	for entry in $checksScroll/vbox.get_children():
+		for i in entry.get_children():
+			if i is CheckBox:
+				checks.append(i)
+	
 func _update() -> void:
 	for i: CheckBox in checks:
 		if not i.button_pressed:
@@ -28,7 +36,6 @@ func _update() -> void:
 
 func _on_text_meta_clicked(meta: String) -> void:
 	OS.shell_open(meta)
-
 
 func _on_btn_proceed_pressed() -> void:
 	if not gui: return
