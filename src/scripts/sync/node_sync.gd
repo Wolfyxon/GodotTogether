4fb2d5886dd79a36721bb3ed2cf64d4c30349556
Wolfyxon <wolfyxon@gmail.com>
2026-09-17 12:26:26 +0200

    checking if element is string

--- src/scripts/sync/node_sync.gd
+++ src/scripts/sync/node_sync.gd
@@ -1235,6 +1235,9 @@ static func _call_setget_entry_method(
 		full_args.append_array(method_entry["post_args"])
 	
 	for i in full_args.size():
+		if full_args[i] is not String:
+			continue
+		
 		if full_args[i] == "?":
 			var k = property.split("/")[1]
 			full_args[i] = k
