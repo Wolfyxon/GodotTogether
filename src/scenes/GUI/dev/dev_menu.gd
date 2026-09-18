17b99bef5ff3c5f25ad240cf7e6462f224297afd
Wolfyxon <wolfyxon@gmail.com>
2026-09-18 11:56:43 +0200

    Prints

--- src/scenes/GUI/dev/dev_menu.gd
+++ src/scenes/GUI/dev/dev_menu.gd
@@ -12,4 +12,19 @@ func _ready() -> void:
 	if not main:
 		return
 	
-	$main/scroll/updateSigningContainer/updateSigning.main = main
+	$main/scroll/vbox/updateSigningContainer/updateSigning.main = main
+
+func _on_btn_node_classes_pressed() -> void:
+	main.debug.dump_node_classes()
+
+func _on_btn_file_tree_pressed() -> void:
+	var paths = GDTFiles.get_file_tree()
+	
+	for i in paths:
+		print(i)
+
+func _on_btn_unsafe_file_tree_pressed() -> void:
+	var paths = GDTFiles.get_file_tree("res://", true)
+	
+	for i in paths:
+		print(i)
