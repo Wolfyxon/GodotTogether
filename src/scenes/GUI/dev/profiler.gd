33af48928d8009f3ed36039028c463b10d90b8d6
Wolfyxon <wolfyxon@gmail.com>
2026-09-19 20:06:27 +0200

    Added change detection performance profiler

---
+++ src/scenes/GUI/dev/profiler.gd
@@ -0,0 +1,69 @@
+@tool
+extends VBoxContainer
+class_name GDTDevMenuProfiler
+
+const MAX_TIME_RESOLUTION = 64
+
+@onready var check: CheckBox = $enabled
+
+var main: GodotTogether = null
+var first_run = true
+
+var node_scan_durations = []
+var file_scan_durations = []
+
+var _last_node_scan_time = 0
+var _last_file_scan_time = 0
+
+func _process(delta: float) -> void:
+	if not main: return
+	if not check.button_pressed: return
+	
+	$nodeCount.text = "Total nodes: %s" % main.node_sync.node_data_dict.size()
+	$nodeTime.text = "Avg node scan time: %s s" % GDTUtils.avg(node_scan_durations)
+	
+	$fileCount.text = "Total files: %s" % main.file_sync.file_hashes.size()
+	$fileTime.text = "Avg file scan time: %s s" % GDTUtils.avg(file_scan_durations)
+	
+
+func setup() -> void:
+	if not main:
+		printerr("Profiler setup failed because main is null") 
+		return
+	
+	main.node_sync.scan_started.connect(_node_scan_started)
+	main.node_sync.scan_complete.connect(_node_scan_complete)
+	
+	main.file_sync.scan_started.connect(_file_scan_started)
+	main.file_sync.scan_complete.connect(_file_scan_complete)
+
+func _node_scan_started() -> void:
+	_last_node_scan_time = Time.get_unix_time_from_system()
+
+func _node_scan_complete() -> void:
+	if _last_node_scan_time == 0: 
+		return
+	
+	var now = Time.get_unix_time_from_system()
+	node_scan_durations.append(now - _last_node_scan_time)
+	
+	if node_scan_durations.size() > MAX_TIME_RESOLUTION:
+		node_scan_durations.pop_front()
+
+func _file_scan_started() -> void:
+	_last_file_scan_time = Time.get_unix_time_from_system()
+
+func  _file_scan_complete() -> void:
+	if _last_file_scan_time == 0:
+		return
+		
+	var now = Time.get_unix_time_from_system()
+	file_scan_durations.append(now - _last_file_scan_time)
+	
+	if file_scan_durations.size() > MAX_TIME_RESOLUTION:
+		file_scan_durations.pop_front()
+
+func _on_enabled_pressed() -> void:
+	if first_run:
+		setup()
+		first_run = false
