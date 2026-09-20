07a8b9b3d07e96ca2bf70222c275be7a2828a246
Wolfyxon <wolfyxon@gmail.com>
2026-09-20 22:59:48 +0200

    Showing if files are initially synced

--- src/scenes/GUI/dev/sync.gd
+++ src/scenes/GUI/dev/sync.gd
@@ -26,6 +26,7 @@ func _process(delta: float) -> void:
 	$fileCount.text = "Total files: %s" % main.file_sync.file_mod_times.size()
 	$fileTime.text = "Avg file scan time: %s s" % GDTUtils.avg(file_scan_durations)
 	$fileCanSync.text = "Is allowed to run: %s" % main.file_sync.can_sync_files()
+	$filesInitSynced.text = "Files initially synced: %s" % main.client.is_fully_synced
 
 func setup() -> void:
 	if not main:
