2e6a1ad1fe605508e75774084b7b37237c5da34b
Wolfyxon <wolfyxon@gmail.com>
2026-09-25 12:59:21 +0200

    Scene logic

--- src/scripts/internal/scene_editor_tabs.gd
+++ src/scripts/internal/scene_editor_tabs.gd
@@ -3,11 +3,20 @@ class_name GDTEditorSceneTabs
 
 var node: Control
 
+var scene_colors := {}
+
 func _wrapper_init() -> void:
 	node = get_editor_node("EditorSceneTabs")
 	if not node: return
 	
 	clear_colors()
+
+# Signals don't work. Must be called in a loop
+func update() -> void:
+	clear_colors(false)
+	
+	for path in scene_colors.keys():
+		set_scene_color(path, scene_colors[path])
 
 func get_tab_bar() -> TabBar:
 	return GDTUtils.get_descendant_with_class(node, "TabBar")
@@ -21,13 +30,18 @@ func get_color_rects() -> Array:
 	
 	return res
 
-func clear_colors() -> void:
+func clear_colors(data := true) -> void:
+	if data:
+		scene_colors.clear()
+	
 	for i in get_color_rects():
 		i.queue_free()
 
+func get_scene_index(path: String) -> int:
+	return EditorInterface.get_open_scenes().find(path)
+
 func set_tab_color(id: int, color: Color) -> void:
 	var tb = get_tab_bar()
-	var rect = tb.get_tab_rect(id)
 	
 	var clr = ColorRect.new()
 	clr.set_meta("gdt_tab_color", true)
@@ -36,8 +50,26 @@ func set_tab_color(id: int, color: Color) -> void:
 	clr.color.a = 0.25
 	
 	tb.add_child(clr)
+	align_color_rect_to_id(id, clr)
+
+func align_color_rect_to_id(id: int, color_rect: ColorRect) -> void:
+	var tb = get_tab_bar()
+	var rect = tb.get_tab_rect(id)
 	
-	clr.position = rect.position
-	clr.size = rect.size
-	clr.visible = true
+	color_rect.position = rect.position
+	color_rect.size = rect.size
+	color_rect.visible = true
+
+func set_scene_color(path: String, color: Color) -> void:
+	scene_colors[path] = color
+	var id = get_scene_index(path)
 	
+	if id != -1:
+		set_tab_color(id, color)
+
+func clear_scene_color(path: String, color: Color) -> void:
+	scene_colors.erase(path)
+	var id = get_scene_index(path)
+	
+	if id != -1:
+		set_tab_color(id, color)
